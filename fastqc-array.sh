ff1caf720cccdc23cbdc2c51b9f1130c3ccb01ed
Michael Beavitt <mbeavitt@gmail.com>
2023-04-29 14:40:57 +0100

    Adding fastqc processing scripts

---
+++ fastqc-array.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+################################################################################
+#                                                                              #
+# Array Job - process all files in a directory                                 #
+#                                                                              #
+################################################################################
+
+# Grid Engine options
+#
+#$ -cwd
+#
+# Task range. Tasks need to go from 1 to the number of files in the target 
+# directory.
+#
+#$ -l h_vmem=4G
+#$ -pe sharedmem 4
+#$ -t 1-48
+#$ -e error
+#$ -o output
+
+# Target directory
+TARGET_DIR=/exports/eddie/scratch/s1653324/transcriptome_data/unzipped_files/raw_data/20220726/raw_sequence_files
+
+# Get list of files in target directory
+files=$(ls -1 ${TARGET_DIR}/)
+
+# Get file to be processed by *this* task 
+# extract the Nth file in the list of files, $files, where N == $SGE_TASK_ID
+this_file=$(echo "${files}" | sed -n ${SGE_TASK_ID}p)
+echo Processing file: ${this_file} on $HOSTNAME
+
+# Process file
+./fastqc-run.sh "${TARGET_DIR}/${this_file}"
